5d70872e1508222273060991d5378968e2d98257
Cameron Ray <cameronray@Camerons-MacBook-Pro.local>
2018-10-29 15:57:03 -0700

    Working on server and then endpoints

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const request = require('supertest');
+
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.status(200).json({ message: 'server is running' });
+});
+
+server.post('/notes', (req, res) => {
+  const newNote = req.body; 
+
+  res.status(200).json(newNote);
+});
+
+module.exports = server;
