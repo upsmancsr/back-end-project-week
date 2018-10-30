7212354fb846a648abe0774347360f22bbc12d11
upsmancsr <cameronsray@gmail.com>
2018-10-30 16:14:37 -0700

    Revert "back-end-project-wee"

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 # back-end-project-week
-first change
-
-Trello URL for this project: https://trello.com/b/WJn1tu0M/lambda-notes-backend
-
--- index.js
+++
@@ -1,4 +0,0 @@
-const server = require('./server.js');
-
-const port = 3300;
-server.listen(port, () => console.log(`\nServer is listening on port ${port}\n`));
